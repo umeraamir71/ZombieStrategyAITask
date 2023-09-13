e06def0d098c05c7eea7cd60781969324b8caf18
Umer Aamir <umeraamir71@gmail.com>
2023-09-13 16:04:52 +0500

    Added CivilianController in civilians

---
+++ ZombieStrategyAI/Assets/Scripts/Controllers/CivilianController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CivilianController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Check if we've reached the destination
+        if (!_agent.pathPending)
+        {
+            if (_agent.remainingDistance <= _agent.stoppingDistance)
+            {
+                if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+                {
+                    SetCivilianDestination();
+                }
+            }
+        }
+    }
+
+    private void SetCivilianDestination()
+    {
+        if (RandomPointToMove(transform.position, 10f, out Vector3 point))
+        {
+            _agent.destination = point;
+        }
+    }
+
+    private bool RandomPointToMove(Vector3 center, float range, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+}
